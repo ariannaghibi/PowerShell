6f42ec66315a5040137671a47a73509640399719
Arian Naghibi <ariannaghibi1988@yahoo.com>
2020-07-05 13:48:22 -0700

    Updated

---
+++ Excel Reader/Excel Reader.ps1
@@ -0,0 +1,13 @@
+﻿#This script reads an Excel file and outputs each Name by their respective color. Total numbers of all objects will also be shown. 
+
+Set-Location "C:\Users\Arian\Desktop\Programming\PowerShell\Excel Reader"
+Import-Csv File.csv
+$OurObjectArray = Import-Csv File.csv
+$TotalOfNumberColumn = 0
+$OurObjectArray | ForEach-Object{
+    Write-Host "The name of the object is:" $_.objectpropertyname -ForegroundColor $_.objectpropertycolor 
+    $TotalOfNumberColumn += $_.objectpropertynumber
+}
+
+Write-Host ""; Write-Host "Total number: " $TotalOfNumberColumn
+
---
+++ Local Drive/Local Drive.ps1
@@ -0,0 +1,11 @@
+﻿#This script lists out all drives on the local machine along with their used, free, and capacity for each. The total free space of all drives will also be shown.
+
+Get-PSDrive | ?{$_.Free -gt 1} | %{
+    $Count = 0
+    Write-Host ""
+    } {$_.Name + ": Used(GB): " + ("{0:N2}" -f ($_.Used/1gb)) + "  Free(GB): " + ("{0:N2}" -f ($_.free/1gb)) + "  Capacity(GB): " + ("{0:N2}" -f (($_.Used + $_.Free/1gb)/1gb))
+    $Count = $Count + $_.Free
+    }{
+Write-Host"";Write-Host "Total Free Space(GB): " ("{0:N2}" -f ($Count/1gb)) -backgroundcolor magenta
+}
+
