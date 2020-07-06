7abe11d3983950c656db597ad79b8af43f29e9b8
Arian Naghibi <ariannaghibi1988@yahoo.com>
2020-07-05 19:12:40 -0700

    updated

---
+++ Ping/Ping.ps1
@@ -0,0 +1,7 @@
+﻿#Test if a computer is online
+
+function ping {
+param($computer_name)
+# test-connection sends pings to a computer once and returns True if connection is established
+return (test-connection $computer_name -Count 1 -Quiet)
+}
